5917f0fe0a4ea9786cb567d92d905e4ab72ea684
Robin Holzner <robin.holzner@epinova.no>
2019-08-24 12:23:56 +0200

    first steg off ruting epi data

---
+++ testEpiCore/Pages/ArticlePage.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EPiServer;
+using EPiServer.Core;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using testEpiCore.EpiServer;
+
+namespace testEpiCore.Pages
+{
+    public class ArticlePage : PageModel
+    {
+        public Models.Pages.ArticlePage epiPage { get;set;}
+        private readonly IActionDescriptorCollectionProvider _actionDescriptorCollectionProvider;
+        private readonly IContentLoader contentLoader;
+        public ArticlePage(IEpiServerInitialize epi, IActionDescriptorCollectionProvider actionDescriptorCollectionProvider)
+        {
+            contentLoader = epi.GetInstance<IContentLoader>();
+            var pages = new List<Models.Pages.ArticlePage>();
+            _actionDescriptorCollectionProvider = actionDescriptorCollectionProvider;
+            //CurrentPage = pages.FirstOrDefault(x => x.URLSegment == )
+        }
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            var routes = _actionDescriptorCollectionProvider.ActionDescriptors.Items.Where(
+            ad => ad.AttributeRouteInfo != null).Select(ad => new RouteModel
+            {
+                Name = ad.AttributeRouteInfo.Name,
+                Template = ad.AttributeRouteInfo.Template
+            }).ToList();
+
+            var path = routes.FirstOrDefault( x => x.Template == Request.Path.Value.Replace("/",""));
+            var hasCurrentPage = contentLoader.TryGet<Models.Pages.ArticlePage>(new Guid(path.Name), out Models.Pages.ArticlePage CurrentPage);
+            epiPage = CurrentPage;
+        }
+    }
+    public class RouteModel
+    {
+        public string Name { get; set; }
+        public string Template { get; set; }
+    }
+}
--- testEpiCore/Startup.cs
+++ testEpiCore/Startup.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using EPiServer;
+using EPiServer.Core;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using StructureMap;
@@ -19,7 +22,7 @@ namespace testEpiCore
     {
         public Startup(IConfiguration configuration)
         {
-        Configuration = configuration;
+            Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
@@ -27,46 +30,95 @@ namespace testEpiCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public IServiceProvider ConfigureServices(IServiceCollection services)
         {
-        services.Configure<CookiePolicyOptions>(options =>
-        {
-        // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-        options.CheckConsentNeeded = context => true;
-        options.MinimumSameSitePolicy = SameSiteMode.None;
-        });
-        services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
 
-        //var epi = new EpiServerInitialize();
-        //services.AddSingleton<IEpiServerInitialize, EpiServerInitialize>();
-        services.Add(new ServiceDescriptor(typeof(IEpiServerInitialize), p => new EpiServerInitialize(), ServiceLifetime.Singleton));
+            //var epi = new EpiServerInitialize();
+            //services.AddSingleton<IEpiServerInitialize, EpiServerInitialize>();
+            var epiCore = new EpiServerInitialize();
+            services.Add(new ServiceDescriptor(typeof(IEpiServerInitialize), p => epiCore, ServiceLifetime.Singleton));
 
-        var container = new Container();
-        container.Configure(config =>
-        {
-        config.Populate(services);
-        });
-        
+            services.AddMvc().AddRazorPagesOptions(o =>
+            {
+                o.Conventions.Add(new EpiPageRouteModelConvention(epiCore));
+            }).SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-        return container.GetInstance<IServiceProvider>();
+
+
+
+            var container = new Container();
+            container.Configure(config =>
+            {
+                config.Populate(services);
+            });
+
+
+            return container.GetInstance<IServiceProvider>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-        if (env.IsDevelopment())
-        {
-        app.UseDeveloperExceptionPage();
-        }
-        else
-        {
-        app.UseExceptionHandler("/Error");
-        app.UseHsts();
-        }
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
 
-        app.UseHttpsRedirection();
-        app.UseStaticFiles();
-        app.UseCookiePolicy();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
 
-        app.UseMvc();
+            app.UseMvc();
+        }
+    }
+    public class EpiPageRouteModelConvention : IPageRouteModelConvention
+    {
+        private readonly EpiServerInitialize _epiCore;
+        public EpiPageRouteModelConvention(EpiServerInitialize epiCore)
+        {
+            _epiCore = epiCore;
+        }
+        public void Apply(PageRouteModel model)
+        {
+
+            var contentLoader = _epiCore.GetInstance<IContentLoader>();
+            var pages = new List<PageData>();
+            foreach (var contentReference in contentLoader.GetDescendents(ContentReference.StartPage))
+            {
+                var hasPage = contentLoader.TryGet(contentReference, out PageData pageData);
+                if (hasPage)
+                {
+                    pages.Add(pageData);
+                }
+            }
+            var gruppedPages = pages.GroupBy(x => x.PageTypeName);
+            foreach (var gruppe in gruppedPages)
+            {
+                if (model.ViewEnginePath.EndsWith(gruppe.Key))
+                {
+                    var versions = new List<string>();
+                    foreach (var item in gruppe)
+                    {
+                        model.Selectors.Add(new SelectorModel()
+                        {
+                            AttributeRouteModel = new AttributeRouteModel
+                            {
+                                Template = item.URLSegment,
+                                Name = item.ContentGuid.ToString(),
+                            }
+                        });
+                    }
+                }
+            }
         }
     }
 }
